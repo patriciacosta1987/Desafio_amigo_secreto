923a2aadb3b6171985c2d642cc9f24b48765cf2c
Patjunicks <patjunicksnina@icloud.com>
2025-03-16 18:27:30 -0300

    Versão  1.2 - código padrão mentoria

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ function adicionarAmigo(){
             amigos.push(nomeAmigo);
             inputAmigo.value = "";
             inputAmigo.focus ();
+            atualizarLista ();
+    }
+function atualizarLista (){
+    let listaAmigos = document.getElementById ('listaAmigos');
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++){
+        let item = document.createElement ('li');
+        item.textContent = amigos[i];
+        listaAmigos.appendChild (item)
+    }
+}
+function sortearAmigo (){
+    if (amigos.length === 0){
+        alert ("Adicione amigos antes de sortear");
+        return;
     }
 
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById ('resultado');
+    resultado.innerHTML = `O amigo sorteado foi: ${amigoSorteado}`;
     
+    let limparLista = document.getElementById ('listaAmigos');
+    limparLista.innerHTML = ""; 
+    amigos = [];
+}
