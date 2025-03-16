a58b56201d8700fcd8485153c7810f22a5bdd94a
Patjunicks <patjunicksnina@icloud.com>
2025-03-16 17:34:19 -0300

    Alerta para nomes vazios

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-const nomeAmigo = document.getElementById('amigo'); // Seleciona o elemento de entrada
-let nome = ''; // Inicializa a variável nome
+let amigos = [];
 
-nomeAmigo.addEventListener('input', function() {
-    nome = nomeAmigo.value;
-  
-    function contemCaracteresInvalidos(nome) {
-      return !/^[a-zA-Z\s-]+$/.test(nome.trim());
-    }
-  
-    if (nome.trim() === "" || contemCaracteresInvalidos(nome)) {
-      alert("Nome inválido. Por favor, preencha o campo com um nome válido (apenas letras, espaços e hífens).");
-      nomeAmigo.value = ''; // Limpa o campo de entrada (opcional)
-      nome = ''; // Reseta a variável nome (opcional)
-    } else {
-      console.log("Nome digitado:", nome);
-    }
-  });
+function adicionarAmigo(){
+    let inputAmigo = document.getElementById ('amigo');
+    let nomeAmigo = inputAmigo.value;
+    console.log (nomeAmigo);
 
-function adicionarAmigo () {
-    console.log ('O botão foi clicado');
-}
+        if (!nomeAmigo) {
+            alert("Digite o nome do amigo");
+            return;
+        }
+            amigos.push(nomeAmigo);
+            inputAmigo.value = "";
+            inputAmigo.focus ();
+    }
+
+    
